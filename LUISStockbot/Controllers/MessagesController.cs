3d8c8a70fc611748cef075c1d8a15962b7311de8
Pritam Shah <pritam.shah@emtecinc.com>
2017-04-10 19:26:30 +0530

    First code

---
+++ LUISStockbot/Controllers/MessagesController.cs
@@ -0,0 +1,118 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Connector;
+using System;
+using Newtonsoft.Json;
+
+namespace LUISStockbot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            ConnectorClient connect = new ConnectorClient(new Uri(activity.ServiceUrl));
+            if (activity.Type == ActivityTypes.Message)
+            {
+
+                string StockRateString;
+                stockLUIS StLUIS = await GetEntityFromLUIS(activity.Text);
+                if (StLUIS.intents.Length > 0)
+                {
+                    switch (StLUIS.intents[0].intent)
+                    {
+                        case "StockPrice":
+                            StockRateString = await GetStock(StLUIS.entities[0].entity);
+                            break;
+                        case "StockPrice2":
+                            StockRateString = await GetStock(StLUIS.entities[0].entity);
+                            break;
+                        default:
+                            StockRateString = "Sorry, I am not getting you...";
+                            break;
+                    }
+                }
+                else
+                {
+                    StockRateString = "Sorry, I am not getting you...";
+                }
+
+                Activity reply = activity.CreateReply(StockRateString);
+                await connect.Conversations.ReplyToActivityAsync(reply);
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+
+        private async Task<string> GetStock(string StockSymbol)
+        {
+            double? dblStockValue = await Yahoobot.GetStockRateAsync(StockSymbol);
+            if (dblStockValue == null)
+            {
+                return string.Format("This \"{0}\" is not an valid stock symbol", StockSymbol);
+            }
+            else
+            {
+                return string.Format("Stock Price of {0} is {1}", StockSymbol, dblStockValue);
+            }
+        }
+
+        private static async Task<stockLUIS> GetEntityFromLUIS(string Query)
+        {
+            Query = Uri.EscapeDataString(Query);
+            stockLUIS Data = new stockLUIS();
+            using (HttpClient client = new HttpClient())
+            {
+                string RequestURI = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/3fe695aa-27f0-4da8-9349-4937895818ce?subscription-key=14bc6d9899ee4e388363e3fff13dc0e5&verbose=true&timezoneOffset=0.0&q=" + Query;
+                HttpResponseMessage msg = await client.GetAsync(RequestURI);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var JsonDataResponse = await msg.Content.ReadAsStringAsync();
+                    Data = JsonConvert.DeserializeObject<stockLUIS>(JsonDataResponse);
+                }
+            }
+            return Data;
+        }  
+
+        private Activity HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                // Handle conversation state changes, like members being added and removed
+                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+                // Not available in all channels
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+            return null;
+        }
+    }
+}
